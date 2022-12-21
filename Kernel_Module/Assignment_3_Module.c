6c737f3a3e4ad57ed5e265341b05e7d89315414e
Himanshu <98085386+Himanshu21464@users.noreply.github.com>
2022-12-21 23:25:13 +0530

    Update Assignment_3_Module.c

--- Kernel_Module/Assignment_3_Module.c
+++ Kernel_Module/Assignment_3_Module.c
@@ -1,50 +1,47 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/sched.h>
-#include <linux/pid.h>
+#include <linux/sched/signal.h>
+#include<linux/string.h>
 #include <linux/path.h>
 
+
+int id=1;
+
+module_param(id,int,S_IRUGO);
+
+
+
+static int tasks_init(void){
+    struct task_struct *process; 
+    struct path *path;
+    char *command_path;
+
+    pr_info("%s: ENtering into KERNEL_MODULE for Assignment_3 \n", __func__);
+    
+    for_each_process(process) {
+
+        
+        if(process->pid==id){
+        get_task_comm(command_path, task);
+        get_task_exe_path(task, &path);
+        d_path(&path->dentry, &path->mnt, command_path, PATH_MAX);
+        printk(KERN_INFO "Process ID       : %d",process->pid);
+        printk(KERN_INFO "User ID          : %d",process->cred->uid);
+        printk(KERN_INFO "Process Group ID : %d",process->cred->gid);
+        printk(KERN_INFO "Command Path     : %s",command_path);
+        
+        printk(KERN_INFO "\n");
+        return 0;
+        }
+         
+    }
+    return 0;
+}
+
+static void tasks_exit(void){
+    pr_info("%s: Exiting module ....\n", __func__);
+}
+
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Himanshu_2021464");
-MODULE_DESCRIPTION("A kernel module that prints the PID, UID, PGID, and command path of a given process");
-
-static int pid = 0;
-module_param(pid, int, 0);
-MODULE_PARM_DESC(pid, "Process ID of the process to print information for");
-
-static int __init print_process_info_init(void)
-{
-struct task_struct *task;
-struct pid *pid_struct;
-struct path *path;
-char *command_path;
-
-printk(KERN_INFO "print_process_info: Initializing module\n");
-
-pid_struct = find_get_pid(pid);
-task = pid_task(pid_struct, PIDTYPE_PID);
-
-if (task == NULL)
-{
-    printk(KERN_INFO "print_process_info: Could not find task with PID %d\n", pid);
-    return -1;
-}
-
-get_task_comm(command_path, task);
-get_task_exe_path(task, &path);
-d_path(&path->dentry, &path->mnt, command_path, PATH_MAX);
-printk(KERN_INFO "Process UID: %d\n", task->pid);
-printk(KERN_INFO "Process UID: %d\n", task->cred->uid);
-printk(KERN_INFO "Process PGID: %d\n", task->group_leader->pgrp);
-printk(KERN_INFO "Process command path: %s\n", command_path);
-
-return 0;
-}
-
-static void __exit print_process_info_exit(void)
-{
-printk(KERN_INFO "print_process_info: Removing module\n");
-}
-
-module_init(print_process_info_init);
-module_exit(print_process_info_exit);
+module_init(tasks_init);
+module_exit(tasks_exit);
