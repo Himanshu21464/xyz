748279fb0746076e8a8a55148326f83ddaec8258
Himanshu <98085386+Himanshu21464@users.noreply.github.com>
2022-12-21 16:35:20 +0530

    Update kmodule.c

--- kmodule.c
+++ kmodule.c
@@ -2,84 +2,68 @@
 #include <linux/module.h>
 #include <linux/syscalls.h>
 
-asmlinkage long custom_syscall(void)
-{
-printk(KERN_INFO "Custom syscall called!\n");
-return 0;
+static unsigned long **aquire_sys_call_table(void) {
+    unsigned long int offset = PAGE_OFFSET;
+    unsigned long **sct;
+
+    while (offset < ULLONG_MAX) {
+        sct = (unsigned long **)offset;
+
+        if (sct[__NR_close] == (unsigned long *) sys_close)
+            return sct;
+
+        offset += sizeof(void *);
+    }
+
+    return NULL;
 }
 
-static unsigned long **find_sys_call_table(void)
-{
-unsigned long **sys_call_table;
-unsigned long int i;
+static void disable_page_protection(void) {
+    unsigned long value;
+    asm volatile("mov %%cr0, %0" : "=r" (value));
 
-for (i = (unsigned long int) sys_close; i < (unsigned long int) &loops_per_jiffy; i += sizeof(void *))
-{
-sys_call_table = (unsigned long **) i;
+    if(!(value & 0x00010000))
+        return;
 
-if (sys_call_table[__NR_close] == (unsigned long *) sys_close)
-  return sys_call_table;
+    asm volatile("mov %0, %%cr0" : : "r" (value & ~0x00010000));
 }
 
-return NULL;
+static void enable_page_protection(void) {
+    unsigned long value;
+    asm volatile("mov %%cr0, %0" : "=r" (value));
+
+    if((value & 0x00010000))
+        return;
+
+    asm volatile("mov %0, %%cr0" : : "r" (value | 0x00010000));
 }
 
-static void disable_page_protection(void)
-{
-unsigned long value;
-asm volatile("mov %%cr0, %0" : "=r" (value));
+static int __init syscall_module_init(void) {
+    sys_call_table = aquire_sys_call_table();
 
-if(!(value & 0x00010000))
-return;
+    if (!sys_call_table) {
+        return -1;
+    }
 
-asm volatile("mov %0, %%cr0" : : "r" (value & ~0x00010000));
+    disable_page_protection();
+    ref_sys_call_table = (void *)sys_call_table[__NR_close];
+    sys_call_table[__NR_close] = (unsigned long *)new_sys_call_table;
+    enable_page_protection();
+
+    return 0;
 }
 
-static void enable_page_protection(void)
-{
-unsigned long value;
-asm volatile("mov %%cr0, %0" : "=r" (value));
+static void __exit syscall_module_exit(void) {
+    if (!sys_call_table) {
+        return;
+    }
 
-if((value & 0x00010000))
-return;
-
-asm volatile("mov %0, %%cr0" : : "r" (value | 0x00010000));
+    disable_page_protection();
+    sys_call_table[__NR_close] = (unsigned long *)ref_sys_call_table;
+    enable_page_protection();
 }
 
-static int __init custom_syscall_init(void)
-{
-unsigned long **sys_call_table;
-
-sys_call_table = find_sys_call_table();
-if (!sys_call_table)
-return -1;
-
-disable_page_protection();
-sys_call_table[__NR_custom_syscall] = (unsigned long *) custom_syscall;
-enable_page_protection();
-
-printk(KERN_INFO "Custom syscall installed!\n");
-return 0;
-}
-
-static void __exit custom_syscall_exit(void)
-{
-unsigned long **sys_call_table;
-
-sys_call_table = find_sys_call_table();
-if (!sys_call_table)
-return;
-
-disable_page_protection();
-sys_call_table[__NR_custom_syscall] = (unsigned long *) NULL;
-enable_page_protection();
-
-printk(KERN_INFO "Custom syscall removed!\n");
-}
-
-module_init(custom_syscall_init);
-module_exit(custom_syscall_exit);
-MODULE_LICENSE("GPL");
-
+module_init(syscall_module_init);
+module_exit(syscall_module_exit);
 
 
