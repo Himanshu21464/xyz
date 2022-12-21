0fb81b7d11623f0ba0082caa91cf69d488580dcc
Himanshu <98085386+Himanshu21464@users.noreply.github.com>
2022-12-21 16:19:20 +0530

    Update kmodule.c

--- kmodule.c
+++ kmodule.c
@@ -1,49 +1,85 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/syscalls.h>
-#include <linux/sched.h>
-#include <linux/uaccess.h>
 
-#define __NR_read_task_struct 356
-
-struct task_struct_data {
-  pid_t pid;
-  uid_t uid;
-  pid_t pgid;
-  char comm[TASK_COMM_LEN];
-};
-
-asmlinkage long sys_read_task_struct(pid_t pid, struct task_struct_data *data)
+asmlinkage long custom_syscall(void)
 {
-  struct task_struct *task;
-
-  task = pid_task(find_vpid(pid), PIDTYPE_PID);
-  if (!task)
-    return -EINVAL;
-
-  data->pid = task->pid;
-  data->uid = task->cred->uid;
-  data->pgid = task->tgid;
-  get_task_comm(data->comm, task);
-
-  return 0;
+printk(KERN_INFO "Custom syscall called!\n");
+return 0;
 }
 
-static int __init init_sys_read_task_struct(void)
+static unsigned long **find_sys_call_table(void)
 {
-  printk("installed new sys_read_task_struct module\n");
-  if (sysptr_read_task_struct == NULL)
-    sysptr_read_task_struct = sys_read_task_struct;
-  return 0;
-}
-static void  __exit exit_sys_read_task_struct(void)
+unsigned long **sys_call_table;
+unsigned long int i;
+
+for (i = (unsigned long int) sys_close; i < (unsigned long int) &loops_per_jiffy; i += sizeof(void *))
 {
-  if (sysptr_read_task_struct != NULL)
-    sysptr_read_task_struct = NULL;
-  printk("removed sys_read_task_struct module\n");
+sys_call_table = (unsigned long **) i;
+
+if (sys_call_table[__NR_close] == (unsigned long *) sys_close)
+  return sys_call_table;
 }
 
-module_init(init_sys_read_task_struct);
-module_exit(exit_sys_read_task_struct);
+return NULL;
+}
 
+static void disable_page_protection(void)
+{
+unsigned long value;
+asm volatile("mov %%cr0, %0" : "=r" (value));
+
+if(!(value & 0x00010000))
+return;
+
+asm volatile("mov %0, %%cr0" : : "r" (value & ~0x00010000));
+}
+
+static void enable_page_protection(void)
+{
+unsigned long value;
+asm volatile("mov %%cr0, %0" : "=r" (value));
+
+if((value & 0x00010000))
+return;
+
+asm volatile("mov %0, %%cr0" : : "r" (value | 0x00010000));
+}
+
+static int __init custom_syscall_init(void)
+{
+unsigned long **sys_call_table;
+
+sys_call_table = find_sys_call_table();
+if (!sys_call_table)
+return -1;
+
+disable_page_protection();
+sys_call_table[__NR_custom_syscall] = (unsigned long *) custom_syscall;
+enable_page_protection();
+
+printk(KERN_INFO "Custom syscall installed!\n");
+return 0;
+}
+
+static void __exit custom_syscall_exit(void)
+{
+unsigned long **sys_call_table;
+
+sys_call_table = find_sys_call_table();
+if (!sys_call_table)
+return;
+
+disable_page_protection();
+sys_call_table[__NR_custom_syscall] = (unsigned long *) NULL;
+enable_page_protection();
+
+printk(KERN_INFO "Custom syscall removed!\n");
+}
+
+module_init(custom_syscall_init);
+module_exit(custom_syscall_exit);
 MODULE_LICENSE("GPL");
+
+
+
